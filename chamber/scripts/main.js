b061403c9a6a50e9b291d745fe9c181b1140d142
Garikai88 <gmangobe@byupathway.edu>
2026-02-02 08:58:50 +0200

    Week 04 Project Assignment

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,12 +1,67 @@
 
-// Set lst modified date 
+// Footer dates
+document.getElementById("year").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = document.lastModified;
 
-// Toggle menu
+// Mobile Menu Toggle
 document.getElementById("menu-toggle").addEventListener("click", () => {
     document.querySelector("nav ul").classList.toggle("show");
 });
 
-// Footer year and last modified
-document.getElementById("year").textContent = new Date().getFullYear();
-document.getElementById("lastModified").textContent = document.lastModified;
+// Modal logic 
+document.querySelectorAll("[data-modal]").forEach(btn => {
+    btn.addEventListener("click", () => {
+        const modalId = btn.dataset.modal;
+        const targetModal = document.getElementById(modalId)
+        if (targetModal) targetModal.style.display = "block";
+    });
+});
+
+document.querySelectorAll(".close").forEach(closeBtn => {
+    closeBtn.addEventListener("click", () => {
+        closeBtn.closest(".modal").style.display = "none";
+    });
+});
+
+
+window.addEventListener("click", e => {
+    if (e.target.classList.contains("modal")) {
+        e.target.style.display = "none";
+    }
+});
+
+// Captire hidden timestamp on Join page
+const timestampInput = document.getElementById("timestamp");
+if (timestampInput) {
+    timestampInput.value = new Date().toLocaleString();
+}
+
+// Function to extract and display the submitted GET data
+function displayFormData() {
+    // Window reference
+    const urlParams = new URLSearchParams(window.location.search)
+
+    // List of required IDs to find in the HTML and match with URL keys
+    const fields = [
+        "firstname",
+        "lastname",
+        "email",
+        "phone",
+        "organization",
+        "timestamp"
+    ];
+
+    fields.forEach(field => {
+        const element = document.getElementById(field);
+        if (element) {
+            const value = urlParams.get(field);
+            element.textContent = value ? decodeURIComponent(value.replace(/\+/g, ' ')) : "Not Provided";
+        }
+    });
+}
+
+// FIXED: Added missing comma and closing qoutes for the event listener 
+document.addEventListener("DOMContentLoaded", displayFormData);
+
+ 
+
